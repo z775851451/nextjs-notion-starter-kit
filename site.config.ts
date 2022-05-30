974b44d2fe803c6ac3b39d55a2eb408dbbb0238e
zhangyuanbo <64820006+z775851451@users.noreply.github.com>
2022-05-30 11:52:20 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3df10106db5b4df8ac13ede5d6866b89',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: '张渊博的个人博客',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: '张渊博',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
